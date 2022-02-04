b615c6a58f75228aaa572caf4db83acc63a9a6f5
Ricard <puigdollersvr@gmail.com>
2022-02-04 16:37:43 +0100

    Update the edit product feature
    Was beeing update by sku, now by ID

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,10 @@
 export const apiGet = (url) => () => fetch(url).then(v => v.json());
 
-export const apiPut = (url, searchKey, searchValue, data) => () => 
+/*
+ * PUT REQUEST - UPDATE BY QUERY
+ * (NOT IN USE) 
+ */
+export const apiPutByQuery = (url, searchKey, searchValue, data) => () => 
     fetch(
         `${url}/${searchKey}/${searchValue}`, {
         method: 'PUT',
@@ -14,6 +18,26 @@ export const apiPut = (url, searchKey, searchValue, data) => () =>
         return r;
     });
 
+/*
+ * PUT REQUEST - UPDATE BY ID
+ */
+export const apiPut = (url, id, data) => () => 
+    fetch(
+        `${url}/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: new Headers({ 'Content-type': 'application/json' })
+    }).then(v => v.json())
+    .then( r => {
+        if(r.error) {
+            return Promise.reject(new Error(r.message));
+        }
+        return r;
+    });
+
+/*
+ *  POST REQUEST - CREATE PRODUCT
+ */
 export const apiPost = (url, data) => () => 
     fetch(`${url}`, {
         method: 'POST',
@@ -27,6 +51,10 @@ export const apiPost = (url, data) => () =>
         return r;
     });
 
+
+/*
+ *  DELETE REQUEST - REMOVE PRODUCT
+ */
 export const apiDelete = (url, searchKey, searchValue) => () => 
     fetch(
         `${url}/${searchKey}/${searchValue}`, {
